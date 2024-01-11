f5e3daa844581ea2aad6c0d8d840e60efa089971
Danii <156008997+daniicode@users.noreply.github.com>
2024-01-11 21:47:33 +0800

    Update PytubeProject.py

--- PytubeProject.py
+++ PytubeProject.py
@@ -8,18 +8,18 @@ def clear_terminal():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def get_user_input():
-    url = input(Fore.CYAN + 'Insert URL: ')
-    quality_preference = input(Fore.CYAN + 'Enter video quality preference (highest/lowest/720p/1080p, etc.): ')
+    url = input(Fore.CYAN + 'YouTube URL: ')
+    quality_preference = input(Fore.CYAN + 'Quality (high/low/720p/1080p): ')
     return url, quality_preference
 
-def download_video(url, quality="highest", folder="PytubeVideos"):
+def download_video(url, quality="high", folder="PytubeVideos"):
     try:
         youtube = YouTube(url)
 
         # Get the appropriate stream based on quality preference
-        if quality == "highest":
+        if quality == "high":
             video_stream = youtube.streams.get_highest_resolution()
-        elif quality == "lowest":
+        elif quality == "low":
             video_stream = youtube.streams.get_lowest_resolution()
         else:
             video_stream = youtube.streams.filter(res=quality).first()
@@ -30,10 +30,12 @@ def download_video(url, quality="highest", folder="PytubeVideos"):
         # Replace spaces with underscores in the video title
         video_title = youtube.title.replace(' ', '_')
 
+        print(Fore.YELLOW + 'Downloading...')
+
         # Download the selected stream to the specified destination or the current working directory
         video_stream.download(folder, filename=video_title + '.' + video_stream.subtype)
 
-        print(Fore.GREEN + f'\nDownload success! Saved to {folder}/{video_title}.{video_stream.subtype}')
+        print(Fore.GREEN + f'\nSucess! Saved to {folder}/{video_title}.{video_stream.subtype}')
         return True
     except Exception as e:
         print(Fore.RED + f'An error has occurred: {e}\nPlease try again.')
