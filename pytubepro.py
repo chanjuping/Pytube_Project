0bcef230aa84a7629dff28b5628821697e185758
Danii <156008997+daniicode@users.noreply.github.com>
2024-01-12 14:55:23 +0800

    Streamlined even more

--- pytubepro.py
+++ pytubepro.py
@@ -9,12 +9,11 @@ def download_video(url, quality="high", folder="PytubeVideos"):
         yt = YouTube(url)
         stream = yt.streams.get_highest_resolution() if quality == "high" else yt.streams.filter(res=quality).first() or yt.streams.filter(res="360p").first()
 
-        os.makedirs(folder, exist_ok=True)
         file_name = f"{yt.title.replace(' ', '_')}_{quality}.{stream.subtype}"
+        os.makedirs(folder, exist_ok=True)
 
         print('\nDownloading...')
         stream.download(folder, filename=file_name)
-
         print(f'\nSuccess! Saved to {folder}/{file_name}')
         return True
     except Exception as e:
